064af73381ff5164310d94b398d07bc49b406e14
hoppa7 <bobgon050@gmail.com>
2025-02-20 17:13:50 +0200

    Finished Vanity_plates

--- CS50 Harvard/Loops/Vanity_plates.py
+++ CS50 Harvard/Loops/Vanity_plates.py
@@ -1,2 +1,26 @@
+import string
+
 def main():
-    pass
+    n = input("Plate: ")
+    if is_valid(n):
+        return "Valid"
+    else:
+        return "Invalid"
+
+
+def is_valid(n):
+    mid_length = int(len(n) / 2)
+    if not n[:2].isalpha():
+        return False
+    if not (2 <= len(n) <= 6):
+        return False
+    if not (n[mid_length:int(len(n))].isnumeric()):
+        return False
+    if any(char in string.punctuation for char in n):
+        return False
+    if n[mid_length].startswith("0"):
+        return False
+    else:
+        return True
+
+print(main())
